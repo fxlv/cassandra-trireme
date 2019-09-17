5504d121cd46df7a3b743db83bc72557e90ea9ba
Kaspars Mickevics <kaspars@fx.lv>
2019-09-17 18:43:14 +0300

    Significant upate. And renaming to Trireme

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# cassandra-row-counter
-How many rows do you have in a table?
+# cassandra-trireme
+
+Count, print and manipulate rows.
+And do that in a distributed way.
 
 ## Why?
+
+This tool solves two main problems.
+
+### Counting rows
 
 Sometimes it is handy to know how many rows of data you have in your Cassandra database.
 Unless you've thought about this when designing your data model, there really is not much choice then to run a full table scan 
@@ -14,38 +20,141 @@ But what if you really need to know the row count.
 Well, then what you can do is to do the counting in smaller batches.
 And that is exactly what this script here does.
 
-## Running
-Without SSL:
 
-```./count.py 127.0.0.1  --user testuser --pass testpass```
+### Updating rows
+
+Sometimes you need to manipulate your data and say something like:
+
+```update test1.testtable1 set something = "new value" where another_criteria = "something"```
+
+You might expect this to work, but it won't. Cassandra won't allow you to do that unless you're filtering by primary key.
+That's less than ideal, and we can talk about data modeling all day, but reality is that in some cases you just need to run this one query.
+
+And that is the other key thing that this tool allows you to do.
+
+## User guide
+
+### A sidenote on SSL
+
+This tool optionally supports SSL, just pass the necessary options to it.
+
+For example, running without SSL:
+
+```./count.py count-rows 172.17.86.138 test1 testtable2 id```
 
 With SSL (certificate and key must be in PEM format):
 
-```./count.py 127.0.0.1 system_auth roles role --port 9142 --user testuser --pass testpass --ssl-certificate client.cer.pem --ssl-key key.pem```
+```./count.py count.py count-rows 172.17.86.138 test1 testtable2 id --ssl-certificate client.cer.pem --ssl-key key.pem```
 
-### Filtering
+For brevity, I will omit the SSL arguments in most examples.
 
-Sometimes you need to do filtering, and this can be done by adding extra conditions to `WHERE` clause.
+### Required positional arguments
 
-This is not a safe thing to do (as there is no sanitization here), but if you must - you can (safety not guaranteed).
+The tool supports multiple actions, such as counting, updating etc.
+For these opeartions to work, you will have to provide some arguments.
 
-In this case we count rows that have `id` field set to integer `1`
+|Argument| Description |
+| --- | --- |
+| host |                  Cassandra host. This can be hostname or IP.|
+|keyspace        |      Keyspace to use|
+|table                 |Table to use|
+|key|                   Key to use. This will be used to calculate token ranges, this should be your primary key.|
+  
+  
+### Counting rows
 
-```./count.py 127.0.0.1 test testtable id --filter-string="id = 1"```
+Counting rows is not something that Cassandra is designed for. But hey, sometimes you just really need this.
+Say, you want to do somethig like: ``` select count(*) from test1.testtable2;```. 
+This will most likely not work for you, as it is inefficient, and takes time. Cassandra will time out. By default the timeout is 5 sec IIRC.
 
-In case you need to filter by a column that is not indexed, you'll have to `ALLOW FILTERING` as follows:
-
-```./count.py 127.0.0.1 test1 testtable id --filter-string="another_unindexed_id = 1 ALLOW FILTERING" --debug```
-
-### Looking at data
-
-Sometimes you just wonder what data you have in there and perhhaps you want to do a query like: hey give me all the rows that have 'some_column' set to `False`.
-Then you could do:
+Solution:
 ```
-./count.py 172.17.86.134 test1 testtable2 id --filter-string="something = False ALLOW FILTERING" --debug --value-column name
+./count.py count-rows 127.0.0.1 test1 testtable2 id
 ```
 
-## Split size
+Please note the `id` here. This is the primary key that we use to compute token ranges.
+
+Complete example:
+```
+C:\Python37\python.exe count.py count-rows 172.17.86.138 test1 testtable2 id
+Total amount of rows in test1.testtable2 is 8
+```
+
+#### Adding filtering conditions
+Now what if you want to ask a different question, like: `select count(*) from test1.testtable2 where some_column = something`.
+This is where `--filter-string` option comes in. It essentially adds a `where x = z` clause.
+
+Let's look at this example:
+```
+ C:\Python37\python.exe count.py count-rows 127.0.0.1 test1 testtable2 id --filter-string="name = 'fx2' allow filtering"
+```
+
+Here we are actually creating a query (token ranges will be different):
+```
+select count(*) from test1.testtable2 where token(id) >= 6776627963145224192 and token(id) < 7776627963145224192 and name = 'fx2' allow filtering
+``` 
+
+Please note the `ALLOW FILTERING` part. This is needed if you want to filter on a column that is not indexed.
+This is not ideal, and not safe, but it does get the job done.
+
+### Printing rows
+
+This is very similar to counting, but, say, you want to actually print them out.
+It would be the same as doing `select * from test1.testtable2 where some_column = something`.
+
+Example:
+```
+ C:\Python37\python.exe count.py print-rows 127.0.0.1 test1 testtable2 id --filter-string="name = 'fx2' allow filtering"
+```
+
+If selecting `*` is a bit too much, you can also restrict it to specific columns by using the `--value-column` option like so:
+
+```
+count.py print-rows 127.0.0.1 test1 testtable2 id --filter-string="name = 'fx2' allow filtering" --value-column=name
+```
+This will print only your `id` (primary key) and the column you specified.
+
+### Updating rows
+
+Sometimes you need to manipulate rows.
+This is same as if you'd want to do: `update test1.testtable2 set name='Olympius' where name = 'fx2';`.
+It won't work, as, again, it would require a full table scan.
+The solution, at least one of solutions, is to do a query to find all rows that match the criteria and then do an update specifically for them.
+
+Let's look at an example:
+```
+C:\Python37\python.exe count.py update-rows 127.0.0.1 test1 testtable2 id --filter-string="name = 'fx1' allow filtering"  --debug --update-key=name --update-value='Olympius'
+```
+
+This will do two things, first it will scan all rows and look for rows where `name = 'fx1'` and note down their `id`.
+Then on second pass it will iterate over the collected set of `id's` and update the column `name` with the new value `Olympius`.
+
+This has been tested with integers, strings and booleans. The script will be clever enough to quote strings in the underlying sql requests.
+
+#### But what if you have compound primary key?
+
+Yes, that is very likely the case in Cassandra world, and it that very case you can provide an extra key by using `--extra-key=registrationid` option.
+
+Example:
+```
+C:\Python37\python.exe count.py update-rows 127.0.0.1 test1 testtable2 id --filter-string="name = 'Olympius' allow filtering"  --debug --update-key=name --update-value='Olympius' --extra-key=second_id
+``` 
+This will do same as the above version, but in the collection phase it will collect both keys, so that during update phase it knows how to find your data.
+
+The update operation will also ask for human confirmation before doing the update run, so it is somewhat _safe_ to run it.
+But of course - _caveat emptor_.
+
+
+
+### Additional information
+
+
+#### Debugging
+
+You can add `--debug` option to get tons of debugging information printed to your `stdout`.
+
+
+#### Split size
 
 Cassandra uses 2^64 partitions, and the complexity of doing the full scan lies in the fact that it has to query all of them to get the row count.
 Now, there might be a better way, but currently seems easiest is just to do many queries by specifying particular range of tokens.
@@ -59,7 +168,16 @@ So, for example for a 4M+ table, number 10^15 works well.
 This is something to improve, and possible some sort of auto detection could be done.
 For now, default is 10^18, and if you need smaller splits, you can specify smaller [powers of 10](https://en.wikipedia.org/wiki/Power_of_10).
 
+You can override defauls split size with the `--split` option. For best results use powers of 10.
+
 ## Current status
 
 This is very much work in progress, currently it works, but isn't pretty.
 Feel free to jump in if you wish.
+
+## The name - Trireme
+
+As this tool mainly deals with different aspects of row counting and manipulation, then it seems rowing motive might be in order.
+And keeping inline with Cassandra, Trireme is a type of ancient greek galley, with three rows of oars. So lots of rowing.
+Hence the name.
+
--- count.py
+++ count.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
+#
+# Cassandra database row counter and manipulator.
+# Need to come up with a better name :)
+#
+# kaspars@fx.lv
+#
 from cassandra.cluster import Cluster
 import argparse
 import sys
 import logging
 from ssl import SSLContext, PROTOCOL_TLSv1_2
 from cassandra.auth import PlainTextAuthProvider
+import threading
+import queue
+import time
+import datetime
 
 min_token = -9223372036854775808
 max_token = 9223372036854775807
@@ -21,6 +31,7 @@ def parse_user_args():
     parser.add_argument("keyspace", type=str, help="Keyspace to use")
     parser.add_argument("table", type=str, help="Table to use")
     parser.add_argument("key", type=str, help="Key to use, when counting rows")
+    parser.add_argument("--extra-key", type=str, dest="extra_key", help="Extra key, in case of compound primary key.")
     parser.add_argument("--update-key", type=str, dest="update_key", help="Update key.")
     parser.add_argument("--update-value", type=str, dest="update_value", help="Update value.")
     parser.add_argument("--value-column", type=str, dest="value_column", help="Value column.")
@@ -71,6 +82,9 @@ def find_null_cells(session, keyspace, table, key_column, value_column):
     Filtering by 'Null' is not something that you can do in Cassandra.
     So what you can do is to query them and look for 'Null' in the result.
     """
+
+    # TODO: this is just a stub for now, not fully implemented
+
     session.execute("use {}".format(keyspace))
 
     sql_template = "select {key},{column} from {keyspace}.{table}"
@@ -84,48 +98,125 @@ def find_null_cells(session, keyspace, table, key_column, value_column):
     # print("Total amount of rows in {keyspace}.{table} is {sum}".format(keyspace=keyspace, table=table, sum=total_sum))
 
 
-def batch_sql_query(sql_statement, key_name, key_list):
+def batch_sql_query(sql_statement, key_name, key_list, dry_run=False):
     """Run a query on the specifies list of primary keys."""
 
     for key in key_list:
-        sql = "{sql_statement} where {key_name} = {key}".format(sql_statement=sql_statement, key_name=key_name, key=key)
+        if isinstance(key, dict):
+            sql = "{sql_statement} where ".format(sql_statement=sql_statement)
+            andcount = 0
+            for k in key:
+                value = key[k]
+                if isinstance(value, str):
+                    value = "'{}'".format(value)
+                sql += "{key_name} = {key}".format(key_name=k, key=value)
+                if andcount < 1:
+                    andcount += 1
+                    sql += " and "
+        else:
+            sql = "{sql_statement} where {key_name} = {key}".format(sql_statement=sql_statement, key_name=key_name,
+                                                                    key=key)
+
         logging.debug("Executing: {}".format(sql))
-        result = session.execute(sql)
-        logging.debug(result)
+        if dry_run:
+            logging.info("Would execute: {}".format(sql))
+        else:
+            result = session.execute(sql)
+            logging.debug(result)
+            time.sleep(0.1)
+
+
+def human_time(seconds):
+    hours = round(seconds / 60 / 60)
+    seconds = seconds - hours * 60 * 60  # remaining seconds
+    minutes = round(seconds / 60)
+    seconds = round(seconds - minutes * 60)
+    return "{} hours, {} minutes, {} seconds".format(hours, minutes, seconds)
+
+
+def sql_query(sql_statement, key_column, result_list, failcount, sql_list, filter_string, kill_queue):
+    while len(sql_list) > 0:
+        if kill_queue.qsize() > 0:
+            logging.warning("Aborting query on request.")
+            return
+        (min, max) = sql_list.pop()
+        sql_base_template = "{sql_statement} where token({key_column}) >= {min} and token({key_column}) < {max}"
+        if filter_string:
+            sql_base_template += " and {}".format(filter_string)
+        sql = sql_base_template.format(sql_statement=sql_statement, min=min, max=max, key_column=key_column)
+        try:
+            if result_list.qsize() % 100 == 0:
+                logging.debug("Executing: {}".format(sql))
+            result = session.execute(sql)
+            result_list.put(result)
+        except Exception as e:
+            failcount += 1
+            logging.warning("Got Cassandra exception: {msg} when running query: {sql}".format(sql=sql, msg=e))
 
 
 def distributed_sql_query(sql_statement, key_column, split, filter_string):
+    start_time = datetime.datetime.now()
     sql_list = []
-    result_list = []
-
-    sql_base_template = "{sql_statement} where token({key_column}) >= {min} and token({key_column}) < {max}"
-
-    if filter_string:
-        logging.debug("using additional filter string: {}".format(filter_string))
-        sql_base_template += " and {}".format(filter_string)
+    result_list = queue.Queue()
+    failcount = 0
+    thread_count = 15
 
     # calculate token ranges for distributing the query
     i = min_token
+    logging.debug("Preparing splits...")
     while i <= max_token - 1:
         i_max = i + pow(10, split)
         if i_max > max_token: i_max = max_token  # don't go higher than max_token
         sql_list.append((i, i_max))
-        logging.debug(i_max)
         i = i_max
-    logging.info("sql list length is {}".format(len(sql_list)))
+    logging.debug("sql list length is {}".format(len(sql_list)))
+    time.sleep(1)
+    process_queue = queue.Queue()
+    kill_queue = queue.Queue()  # TODO: change this to an event?
 
-    for (min, max) in sql_list:
-        sql = sql_base_template.format(sql_statement=sql_statement, min=min, max=max, key_column=key_column)
-        logging.debug("Executing: {}".format(sql))
-        result = session.execute(sql)
-        result_list.append(result)
+    tm = None
+    try:
+        while len(sql_list) > 0:
+            if process_queue.qsize() < thread_count:
+                thread = threading.Thread(target=sql_query, args=(
+                sql_statement, key_column, result_list, failcount, sql_list, filter_string, kill_queue))
+                thread.start()
+                logging.debug("Started thread {}".format(thread))
+                process_queue.put(thread)
+            else:
+                logging.debug("Max process count reached")
+                logging.debug("{} more queries remaining".format(len(sql_list)))
+                res_count = result_list.qsize()
+                logging.debug("{} results so far".format(res_count))
+                n = datetime.datetime.now()
+                delta = n - start_time
+                elapsed_time = delta.total_seconds()
+                logging.debug("Elapsed time: {}.".format(human_time(elapsed_time)))
+                if res_count > 0:
+                    result_per_sec = res_count / elapsed_time
+                    logging.debug("{} results / s".format(result_per_sec))
+                time.sleep(10)
+        logging.debug("No more work left, waiting for all threads to stop.")
+        # TODO: maybe instead send the kill pill or check thread liveliness in some other way
+        # instead of just blindly waiting for a sec
+        time.sleep(1)
+    except KeyboardInterrupt:
+        logging.warning("Ctrl+c pressed, asking all threads to stop.")
+        kill_queue.put(0)
+        time.sleep(2)
+        logging.debug("{} more queries remaining".format(len(sql_list)))
+        logging.debug("{} results so far".format(res_count))
+
+    if failcount > 0:
+        logging.warning("There were {} failures during the query.".format(failcount))
     return result_list
 
 
 def reductor(result_set):
     """Do the reduce part of map/reduce and return a list of rows."""
     result_list = []
-    for result in result_set:
+    while result_set.qsize() > 0:
+        result = result_set.get()
         for row in result:
             result_list.append(row)
     return result_list
@@ -139,7 +230,7 @@ def delete_rows(session, keyspace, table, key, split, filter_string):
     return (reductor(result))
 
 
-def update_rows(session, keyspace, table, key, update_key, update_value, split, filter_string):
+def update_rows(session, keyspace, table, key, update_key, update_value, split, filter_string, extra_key=None):
     """Update specified rows by setting 'update_key' to 'update_value'.
 
     When Updating rows in Cassandra you can't filter by token range.
@@ -147,30 +238,53 @@ def update_rows(session, keyspace, table, key, update_key, update_value, split, 
     we would like to update, and then run an upadate in a for loop.
     """
     session.execute("use {}".format(keyspace))
-
-    rows = get_rows(session, keyspace, table, key, split, update_key, filter_string)
+    rows = get_rows(session, keyspace, table, key, split, update_key, filter_string, extra_key)
     update_list = []
     for row in rows:
-        update_list.append(getattr(row, key))
+        if extra_key:
+            update_list.append(
+                {key: getattr(row, key), extra_key: getattr(row, extra_key)})  # use tuple of key, extra_key
+        else:
+            update_list.append(getattr(row, key))
     logging.info("Updating {} rows".format(len(update_list)))
     logging.info(
         "Updating rows and setting {update_key} to new value {update_value} where filtering string is: {filter_string}".format(
             update_key=update_key, update_value=update_value, filter_string=filter_string))
 
+    # surround update value with quotes in case it is a string, but don't do it if it looks like a string
+    # but in reality is meant to be a a boolean
+    booleans = ["true", "false"]
+    if isinstance(update_value, str):
+        if update_value.lower() not in booleans:
+            update_value = "'{}'".format(update_value)
+
     sql_template = "update {keyspace}.{table} set {update_key} = {update_value}"
     sql_statement = sql_template.format(keyspace=keyspace, table=table, update_key=update_key,
                                         update_value=update_value)
-    result = batch_sql_query(sql_statement, key, update_list)
+    logging.info(sql_statement)
+
+    while True:
+        response = input("Are you sure you want to continue? (y/n)").lower().strip()
+        if response == "y":
+            break
+        elif response == "n":
+            logging.warning("Aborting upon user request")
+            return 1
+    result = batch_sql_query(sql_statement, key, update_list, False)
     logging.info("Operation complete.")
 
 
-def get_rows(session, keyspace, table, key, split, value_column=None, filter_string=None):
+def get_rows(session, keyspace, table, key, split, value_column=None, filter_string=None, extra_key=None):
     session.execute("use {}".format(keyspace))
 
     if not value_column:
         select_values = "*"
     else:
-        select_values = "{key}, {value_column}".format(key=key, value_column=value_column)
+        if extra_key:
+            select_values = "{key}, {extra_key}, {value_column}".format(key=key, extra_key=extra_key,
+                                                                        value_column=value_column)
+        else:
+            select_values = "{key}, {value_column}".format(key=key, value_column=value_column)
 
     sql_template = "select {select_values} from {keyspace}.{table}"
 
@@ -187,7 +301,8 @@ def get_rows_count(session, keyspace, table, key, split, filter_string=None):
     sql_statement = sql_template.format(keyspace=keyspace, table=table)
     result = distributed_sql_query(sql_statement, key_column=key, split=split, filter_string=filter_string)
     count = 0
-    for r in result:
+    while result.qsize() > 0:
+        r = result.get()
         count += r[0].count
     return count
 
@@ -224,8 +339,7 @@ if __name__ == "__main__":
         print_rows(session, args.keyspace, args.table, args.key, args.split, args.value_column, args.filter_string)
     elif args.action == "update-rows":
         update_rows(session, args.keyspace, args.table, args.key, args.update_key, args.update_value, args.split,
-                    args.filter_string)
-
+                    args.filter_string, args.extra_key)
     elif args.action == "delete-rows":
         delete_rows(session, args.keyspace, args.table, args.key, args.split, args.filter_string)
 
