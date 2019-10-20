9b5d047819436bb4ef57ada9afba0810c96cbe50
Kaspars Mickevics <kaspars@fx.lv>
2019-10-20 14:07:55 +0300

    fix human_time()

--- count.py
+++ count.py
@@ -147,13 +147,34 @@ def batch_sql_query(sql_statement, key_name, key_list, dry_run=False):
             logging.debug(result)
             time.sleep(0.1)
 
+def seconds_to_human(seconds):
+    # default values
+    hours = 0
+    minutes = 0
+
+    # find minutes and the reminder of seconds
+    if seconds >= 60:
+        remaining_seconds = seconds % 60
+        minutes = round(( seconds - remaining_seconds ) / 60)
+        seconds = remaining_seconds
+    if minutes >= 60:
+        remaining_minutes = minutes % 60
+        hours = round(( minutes - remaining_minutes ) / 60)
+        minutes = remaining_minutes
+    return hours, minutes, seconds
 
 def human_time(seconds):
-    hours = round(seconds / 60 / 60)
-    seconds = seconds - hours * 60 * 60  # remaining seconds
-    minutes = round(seconds / 60)
-    seconds = round(seconds - minutes * 60)
-    return "{} hours, {} minutes, {} seconds".format(hours, minutes, seconds)
+
+    hours, minutes, seconds = seconds_to_human(seconds)
+
+    if hours:
+        human_time_string = "{} hours, {} minutes, {} seconds".format(hours, minutes, seconds)
+    elif minutes:
+        human_time_string = "{} hours, {} minutes, {} seconds".format(hours, minutes, seconds)
+    else:
+        human_time_string = "{} hours, {} minutes, {} seconds".format(hours, minutes, seconds)
+
+    return human_time_string
 
 
 def sql_query(sql_statement, key_column, result_list, failcount, sql_list, filter_string, kill_queue):
